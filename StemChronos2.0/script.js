102f15ef237ca304ad8d417cffaf00087cdf5c6c
Evan <24ehughes@student.dasd.org>
2023-11-26 12:02:04 -0500

    BigUpdate

--- StemChronos2.0/script.js
+++ StemChronos2.0/script.js
@@ -1,178 +1,249 @@
 
 
-const BaseUrl = 'http://69.242.41.167:8082';
+// Import the functions you need from the SDKs you need
 
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const d = new Date();
+const DayOfWeek = d.getDay();
+
+const DailyMessageOBJ = document.getElementById('DailyMessageText');
+const DailyMessageHEADER = document.getElementById('DailyMessageHeader');
+
+
+const DaysOfWeekAbrv = ['Sun', 'Mon', 'Tue', 'Wen', 'Thu', 'Fri', 'Sat']
+const DaysOfWeekFull = ['Sunday', 'Monday', 'Tuesday',  'Wednesday', 'Thursday', 'Friday', 'Saturday']
 const AllMonths = ["January", "Febuary", "March", "April", "May", "June", "July", "Agust", "September", "October", "November", "December"];
-async function GetData() {
+
+const LetterDayOBJ = document.getElementById('LetterDayParent');
+const WeekendImageOBJ = document.getElementById('WeekendImage');
+
+function GetData() {
+	
+	if (DayOfWeek == 0 || DayOfWeek == 6) {
+		DailyMessageHEADER.innerHTML = "Enjoy your " + DaysOfWeekFull[DayOfWeek];
+		LetterDayOBJ.style.display = 'none';
+		WeekendImageOBJ.style.display = 'revert';
+		return
+    }
+
+	const firebaseConfig = {
+		apiKey: "AIzaSyBL9jpJTz-eCYJM0r1etRbx_6YpPqa67_4",
+		authDomain: "chronos-96d4f.firebaseapp.com",
+		databaseURL: "https://chronos-96d4f.firebaseio.com",
+		projectId: "chronos-96d4f",
+		storageBucket: "chronos-96d4f.appspot.com",
+		messagingSenderId: "866705537581",
+		appId: "1:866705537581:web:5ba4523272685649c116a4",
+		measurementId: "G-RK77WZHGE9"
+	};
+
+	// Initialize Firebase
+	const app = firebase.initializeApp(firebaseConfig);
+
+	const db = firebase.firestore(app);
+
 	var userYear = localStorage.getItem("Year");
-	if (userYear ==null) {
+	if (userYear == null) {
 		alert("Choose your grade level");
-		localStorage.setItem("Year", "FRE")
+		localStorage.setItem("Year", "Fresh")
+		userYear = localStorage.getItem("Year")
 	}
 	document.getElementById("GradeLevel").value = localStorage.getItem("Year");
 	document.getElementById("GradeLevel").addEventListener("change", () => {
 
 		localStorage.setItem("Year", document.getElementById("GradeLevel").value)
-		
-		GetData();
+		location.reload();
+
 	});
 
+
+	//const docRef = firebase.doc();
+	db.collection("Schedule").doc(userYear.toString()).get().then(docSnap => {
+		if (docSnap.exists) {
+			console.log("Document data:", docSnap.data());
+			SetSchedule(docSnap.data());
+		} else {
+			// docSnap.data() will be undefined in this case
+			console.log("No such document!");
+		}
+	}
+	);
+}
+
+GetData();
+
+
+
+async function SetSchedule(Schedule) {
 	
+	let hour = d.getHours();
+	let minutes = d.getMinutes();
+	var LetterDay = Schedule[DaysOfWeekAbrv[DayOfWeek - 1] + "Letter"]
+	if (LetterDay=="X") {
+		return;
+    }
+
+
+	var PeriodNames = [];
+	var PeriodTimes = [];
+
 	try {
 
-		await fetch(BaseUrl + "/api/Schedule/" + localStorage.getItem("Year"), {
-			method: "GET",
-			headers: {
-				Accept: "application/json",
-			},
-		})
-			.then((response) => response.json())
-			.then((json) => {
-				const d = new Date();
-				let hour = d.getHours();
-				let minutes = d.getMinutes();
+	
 			
-				SetDateAndTime();
-				
-				var length = json[0].classOrder.length;
-				
-				var firstHour;
-				var lastHour;
-				var firstMinute;
-				var lastMinute;
-
-				const Remianing = document.getElementById("TimeRemaining");
-				var table = document.getElementById("Schedule");
-				AddNewHeader(table);
-				
-				document.getElementById("LetterDay").innerHTML = json[0].letterDay;
+		
 			
-				var foundCurrentPeriod=false;
-				for (var tempIndex = 0; tempIndex < length; tempIndex++) {
-					
-					var currentPeriod = false;
-					var startHour = json[0].startTime[tempIndex].slice(0, 2);
-					var endHour = json[0].endTime[tempIndex].slice(0, 2);
-					var endMinute = json[0].endTime[tempIndex].slice(3, 5);
-					var startMinute = json[0].startTime[tempIndex].slice(3, 5);
+		SetDateAndTime();
+		
+		
+		PeriodNames = Schedule[DaysOfWeekAbrv[DayOfWeek] + "Periods"];
+		PeriodTimes = Schedule[DaysOfWeekAbrv[DayOfWeek] + "Times"];
+		
+		console.log(PeriodNames);
+		
+		
+		
+		
+			var firstHour ;
+			
+			var lastHour ;
+			var firstMinute ;
+			var lastMinute ;
+			
 
-					if (tempIndex == 0) {
-						
-						firstHour = startHour;
-						firstMinute = startMinute;
+			const Remianing = document.getElementById("TimeRemaining");
+			var table = document.getElementById("Schedule");
+			AddNewHeader(table);
+
+		document.getElementById("LetterDay").innerHTML = LetterDay;
+
+			var foundCurrentPeriod = false;
+			for (var i = 0; i < PeriodNames.length; i++) {
+				console.log(i)
+				var currentPeriod = false;
+				var startHour = PeriodTimes[i].slice(0, 2);
+				var endHour = PeriodTimes[i].slice(6, 8);
+				var endMinute = PeriodTimes[i].slice(9, 11);
+				var startMinute = PeriodTimes[i].slice(3, 5);
+				var PeriodName = PeriodNames[i];
+				if (i == 0) {
+
+					firstHour = startHour;
+					firstMinute = startMinute;
+				}
+
+
+				var hoursLeft;
+				var minutesLeft;
+				if (hour >= startHour && hour <= endHour && !foundCurrentPeriod) {
+					if (hour == endHour && hour == startHour) {
+						if (minutes < endMinute && minutes >= startMinute) {
+							//console.log("In range test 0")
+							currentPeriod = true;
+						}
 					}
-					
-					
-					var hoursLeft;
-					var minutesLeft;
-					if (hour >= startHour && hour <= endHour &&!foundCurrentPeriod) {
-						if (hour == endHour && hour == startHour) {
-							if (minutes < endMinute && minutes >= startMinute) {
-								//console.log("In range test 0")
-								currentPeriod = true;
-							}
-						}
-						else if (hour == endHour && minutes < endMinute) {//if the hour is just equal to the end hour
-							
-							currentPeriod = true;
-							//console.log("ran3")
-						}
-						else if (hour == startHour && minutes >= startMinute) {
-							//console.log("ran2")
-							currentPeriod = true;
-						} else if (hour != startHour && hour != endHour) {
-							currentPeriod = true;
+					else if (hour == endHour && minutes < endMinute) {//if the hour is just equal to the end hour
+
+						currentPeriod = true;
+						//console.log("ran3")
+					}
+					else if (hour == startHour && minutes >= startMinute) {
+						//console.log("ran2")
+						currentPeriod = true;
+					} else if (hour != startHour && hour != endHour) {
+						currentPeriod = true;
 						//	console.log("ran1")
-						}
-						if (currentPeriod) {
-
-
-							hoursLeft = endHour - hour;
-							minutesLeft = endMinute - minutes;
-
-
-							if (minutesLeft < 0) {
-								hoursLeft -= 1;
-								minutesLeft += 60;
-							}
-							Remianing.innerHTML = "There is "
-
-							if (hoursLeft == 1) {
-								Remianing.innerHTML += hoursLeft + " hour ";
-							} else if (hoursLeft > 1) {
-								Remianing.innerHTML += hoursLeft + " hour ";
-							}
-
-							if (minutes > 1 || minutes == 0) {
-								Remianing.innerHTML += minutesLeft + " minutes left in";
-							} else {
-								Remianing.innerHTML += minutesLeft + " minute left in";
-							}
-							console.log(json[0].classOrder[tempIndex].replace(/\s+/g, ''));
-							if (json[0].classOrder[tempIndex].replace(/\s+/g, '') != "Advisory" && json[0].classOrder[tempIndex].replace(/\s+/g, '') != "Seminar" && json[0].classOrder[tempIndex].replace(/\s+/g, '') != "Lunch" && json[0].classOrder[tempIndex].replace(/\s+/g, '') != "L&L") {
-								Remianing.innerHTML += " " + json[0].classOrder[tempIndex] + " Period"
-							} else {
-								Remianing.innerHTML += " " + json[0].classOrder[tempIndex]
-
-							}
-						}
-					}//end of within range
-					let tr = document.createElement('tr');
-					let TitleTR = document.createElement('td');
-					
-					TitleTR.innerHTML = json[0].classOrder[tempIndex];
-					let TimeTR = document.createElement('td');
-					TimeTR.innerHTML = json[0].startTime[tempIndex].slice(0, 5) + "-" + json[0].endTime[tempIndex].slice(0, 5);
-					
-					if (currentPeriod) {
-						
-						TitleTR.classList.add("CurrentPeriod");
-						TimeTR.classList.add("CurrentPeriod");
-						
-						//TitleTR.classList.add("CurrentPeriod");
-						//TimeTR.classList.add("CurrentPeriod");
-						
-					
-					} 
-					
-					tr.appendChild(TitleTR);
-					tr.appendChild(TimeTR);
-					
-					table.appendChild(tr);
-					
-				} 
-				if (hour < firstHour || hour == firstHour && minutes < firstMinute) {
-
-					var minutesUnitilSchoolStart = ((firstHour * 60) + parseInt(firstMinute)) - ((hour * 60) + minutes)
-
-					Remianing.innerHTML = "School starts in "
-					if (minutesUnitilSchoolStart / 60 < 1) {
-						Remianing.innerHTML += minutesUnitilSchoolStart + " minutes"
-					} else {
-						var hoursLeft = minutesUnitilSchoolStart - ((minutesUnitilSchoolStart % 60))
-						hoursLeft /= 60;
-						if (hoursLeft > 1) {
-							Remianing.innerHTML += hoursLeft + " hours " + (minutesUnitilSchoolStart % 60) + " minutes"
-						} else {
-							Remianing.innerHTML += hoursLeft + " hour " + (minutesUnitilSchoolStart % 60) + " minutes"
-						}
-
 					}
-				
-				} else if (hour > lastHour || hour == lastHour && minutes > lastMinute) {
-					Remianing.innerHTML = "Schools over!"
-					
-				} 
-			})
-			.catch(error => {
-				console.error(error);
-			});
+					if (currentPeriod) {
+
+
+						hoursLeft = endHour - hour;
+						minutesLeft = endMinute - minutes;
+
+
+						if (minutesLeft < 0) {
+							hoursLeft -= 1;
+							minutesLeft += 60;
+						}
+						Remianing.innerHTML = "There is "
+
+						if (hoursLeft == 1) {
+							Remianing.innerHTML += hoursLeft + " hour ";
+						} else if (hoursLeft > 1) {
+							Remianing.innerHTML += hoursLeft + " hour ";
+						}
+
+						if (minutes > 1 || minutes == 0) {
+							Remianing.innerHTML += minutesLeft + " minutes left in";
+						} else {
+							Remianing.innerHTML += minutesLeft + " minute left in";
+						}
+						//console.log(json[0].classOrder[i].replace(/\s+/g, ''));
+						if (PeriodName != "Advisory" && PeriodName != "Seminar" && PeriodName != "Lunch" && PeriodName != "L&L") {
+							Remianing.innerHTML += " " + PeriodName + " Period"
+						} else {
+							Remianing.innerHTML += " " + PeriodName
+
+						}
+					}
+				}//end of within range
+				let tr = document.createElement('tr');
+				let TitleTR = document.createElement('td');
+
+				TitleTR.innerHTML = PeriodName
+				let TimeTR = document.createElement('td');
+				TimeTR.innerHTML = PeriodTimes[i];
+
+				if (currentPeriod) {
+
+					TitleTR.classList.add("CurrentPeriod");
+					TimeTR.classList.add("CurrentPeriod");
+
+					//TitleTR.classList.add("CurrentPeriod");
+					//TimeTR.classList.add("CurrentPeriod");
+
+
+				}
+
+				tr.appendChild(TitleTR);
+				tr.appendChild(TimeTR);
+
+				table.appendChild(tr);
+
+			}
+			if (hour < firstHour || hour == firstHour && minutes < firstMinute) {
+
+				var minutesUnitilSchoolStart = ((firstHour * 60) + parseInt(firstMinute)) - ((hour * 60) + minutes)
+
+				Remianing.innerHTML = "School starts in "
+				if (minutesUnitilSchoolStart / 60 < 1) {
+					Remianing.innerHTML += minutesUnitilSchoolStart + " minutes"
+				} else {
+					var hoursLeft = minutesUnitilSchoolStart - ((minutesUnitilSchoolStart % 60))
+					hoursLeft /= 60;
+					if (hoursLeft > 1) {
+						Remianing.innerHTML += hoursLeft + " hours " + (minutesUnitilSchoolStart % 60) + " minutes"
+					} else {
+						Remianing.innerHTML += hoursLeft + " hour " + (minutesUnitilSchoolStart % 60) + " minutes"
+					}
+
+				}
+
+			} else if (hour > lastHour || hour == lastHour && minutes > lastMinute) {
+				Remianing.innerHTML = "Schools over!"
+
+			}
+		
 	}
 	catch (e) {
 		console.log(e);
 	}
 }
-GetData();
+
 
 function AddNewHeader(table) {
 	table.innerHTML = "";
@@ -206,4 +277,25 @@ function SetDateAndTime() {
 		document.getElementById("CurrentTime").innerHTML = hour + ":" + minutes
 	}
 
+}
+
+
+
+function SortArray(ArrayToSort) {
+	var tempArray = [];
+	
+	
+		
+		for (var i = 0; i < ArrayToSort.length; i++) {
+			
+			
+			if (ArrayToSort[i] != null && ArrayToSort[i] != "" ) {
+				tempArray.push(ArrayToSort[i]);
+			} else {
+				break;
+            }
+        }
+		
+	
+	return tempArray;
 }
