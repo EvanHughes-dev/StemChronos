10a38f3d34aa488fe72cdc1ecd666db885387bd9
Evan <24ehughes@student.dasd.org>
2023-11-28 17:20:03 -0500

    update

--- StemChronos2.0/Settings/Settings.js
+++ StemChronos2.0/Settings/Settings.js
@@ -38,5 +38,40 @@ function SetDateAndTime() {
 	}
 
 }
+const PeriodNameOBJ = document.getElementById("PeriodName");
+const PeriodBtnOBJ = document.getElementById("PeriodToggle");
 
+const PeriodID = ["FirstVal", 'SecondVal', 'ThirdVal', 'FourthVal', 'FiffthVal', 'SixthVal', 'SeventhVal']
+
+function TogglePeriod() {
+	if (localStorage.getItem("Period1") != null) {
+		for (var i = 0; i < 6;i++) {
+			var tempI = i + 1;
+			document.getElementById(PeriodID[i]).value = localStorage.getItem("Period" + tempI);
+        }
+    }
+	if (PeriodNameOBJ.style.display == "initial") {
+		PeriodNameOBJ.style.display = "none";
+		PeriodBtnOBJ.innerHTML ="Edit Period Names"
+	} else {
+		PeriodNameOBJ.style.display = "initial";
+		PeriodBtnOBJ.innerHTML = "Stop Editing"
+    }
+}
+
+function SavePeriodNames() {
+	for (var i = 0; i < 7; i++) {
+	
+		if (document.getElementById(PeriodID[i]).value == null || document.getElementById(PeriodID[i]).value =="") {
+			alert("You must fill all periods with a name")
+			return;
+        }
+	}
+	for (var i = 1; i <= 6; i++) {
+		console.log(document.getElementById(PeriodID[i]).value +" "+ i)
+		localStorage.setItem("Period" + i, document.getElementById(PeriodID[i-1]).value);
+		 
+	}
+
+}
 Initial()
