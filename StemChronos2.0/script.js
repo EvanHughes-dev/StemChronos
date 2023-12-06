3570a71397877142db7d77f312060aae272c49bb
EHughes <ejhingm@comcast.net>
2023-12-06 17:12:04 -0500

    header update

--- StemChronos2.0/script.js
+++ StemChronos2.0/script.js
@@ -217,41 +217,41 @@ function AddNewElement(table, PeriodName, PeriodTime, currentPeriod) {
 			PeriodName = window.localStorage.getItem('Period' + PeriodName.substring(0, 1));
         }
     }
-	let tr = document.createElement('tr');
+				let tr = document.createElement('tr');
 				let TitleTR = document.createElement('td');
 				
 				TitleTR.innerHTML = PeriodName
-	let TimeTR = document.createElement('td');
+				let TimeTR = document.createElement('td');
 
-	var TimeMode = localStorage.getItem("TimeMode");
-			if (TimeMode == null || TimeMode == "12Hour") {
-				var startHour = PeriodTime.slice(0, 2);
-				var endHour = PeriodTime.slice(6, 8);
-				var endMinute = PeriodTime.slice(9, 11);
-				var startMinute = PeriodTime.slice(3, 5);
-				var StartTime_AmPm ="AM";
-				var EndTime_AmPm = "AM";
-				if (startHour >= 12) {
-					SstartTime_AmPm = "PM"
-					if (startHour >= 13) {
-						startHour -= 12;
-                    }
-				}
-
-				if (endHour >= 12) {
-					EndTime_AmPm = "PM"
-					if (endHour >= 13) {
-						endHour -= 12;
-						if (endHour < 10) {
-							endHour = "0" + endHour;
-                        }
+				var TimeMode = localStorage.getItem("TimeMode");
+				if (TimeMode == null || TimeMode == "12Hour") {
+					var startHour = PeriodTime.slice(0, 2);
+					var endHour = PeriodTime.slice(6, 8);
+					var endMinute = PeriodTime.slice(9, 11);
+					var startMinute = PeriodTime.slice(3, 5);
+					var StartTime_AmPm ="AM";
+					var EndTime_AmPm = "AM";
+					if (startHour >= 12) {
+						SstartTime_AmPm = "PM"
+						if (startHour >= 13) {
+							startHour -= 12;
+						}
 					}
+
+					if (endHour >= 12) {
+						EndTime_AmPm = "PM"
+						if (endHour >= 13) {
+							endHour -= 12;
+							if (endHour < 10) {
+								endHour = "0" + endHour;
+							}
+						}
+					}
+					console.log()
+					TimeTR.innerHTML = startHour + ":" + startMinute + " " + StartTime_AmPm + " - " + endHour + ":" + endMinute + " " + EndTime_AmPm;
+				} else {
+					TimeTR.innerHTML = PeriodTime;
 				}
-				console.log()
-				TimeTR.innerHTML = startHour + ":" + startMinute + " " + StartTime_AmPm + " - " + endHour + ":" + endMinute + " " + EndTime_AmPm;
-			} else {
-				TimeTR.innerHTML = PeriodTime;
-			}
 				
 
 				if (currentPeriod) {
@@ -312,10 +312,23 @@ function SetDateAndTime() {
 
 	document.getElementById("CurrentDate").innerHTML = AllMonths[month] + " " + day + ", " + year
 
+	var TimeMode = localStorage.getItem("TimeMode");
+	var AM_PM ="AM"
+	if (TimeMode == null || TimeMode == "12Hour") {
+		if(hour>=12){
+			AM_PM="PM";
+		}
+		if(hour>=13){
+			hour-=12;
+		}
+	}else{
+		AM_PM="";
+	}
+
 	if (minutes < 10) {
-		document.getElementById("CurrentTime").innerHTML = hour + ":0" + minutes
+		document.getElementById("CurrentTime").innerHTML = hour + ":0" + minutes +" " + AM_PM
 	} else {
-		document.getElementById("CurrentTime").innerHTML = hour + ":" + minutes
+		document.getElementById("CurrentTime").innerHTML = hour + ":" + minutes + " " + AM_PM
 	}
 
 }
@@ -329,7 +342,11 @@ function FetchMessageData() {
 	//const docRef = firebase.doc();
 	db.collection("Schedule").doc("DailyMessage").get().then(docSnap => {
 		if (docSnap.exists) {
-			SetDailyMessage(docSnap.data())
+
+			if(docSnap.data()[0]!=null){
+				SetDailyMessage(docSnap.data())
+			}
+			
 
 		} else {
 			// docSnap.data() will be undefined in this case
